b17ce6fb8c4da2d8129bf7cd3a4f07de2367299d
Joe Heyes <josephheyes@gmail.com>
2024-01-27 12:35:25 +0000

    Create initial sensing test

---
+++ ye-olde-controllers/sensor.py
@@ -0,0 +1,11 @@
+from gpiozero import DistanceSensor
+
+class SensorController:
+
+    def __init__(self, in_range_cb, out_of_range_cb) -> None:
+        self.sensor = DistanceSensor(echo=4, trigger=6, threshold_distance=0.2)
+        self.sensor.when_in_range = in_range_cb
+        self.sensor.when_out_of_range = out_of_range_cb
+
+    def set_threshold(self, distance: float):
+        self.sensor.threshold_distance = distance
