c2f62d426947585e59922af8453c519a62e9b645
Joe Heyes <josephheyes@gmail.com>
2024-01-27 19:19:55 +0000

    create MagnaKartaController for managing front and back wheels

---
+++ ye-olde-src/magna_karta_controller.py
@@ -0,0 +1,25 @@
+from gpiozero import Robot
+
+class MagnaKartaController:
+    
+    def __init__(self, speed: float) -> None:
+        self.back = Robot(left=(22, 23), right=(9, 25))
+        self.front = Robot(left=(), right=())
+        self.speed = speed
+
+    def forward(self):
+        self.back.forward(self.speed)
+        self.front.forward(self.speed)
+
+    def backward(self):
+        self.back.backward(self.speed)
+        self.front.backward(self.speed)
+
+    def left(self):
+        self.back.left(self.speed)
+        self.front.left(self.speed)
+    
+    def right(self):
+        self.back.right(self.speed)
+        self.front.right(self.speed)
+
