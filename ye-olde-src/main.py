ac074cf86ba0bca9876619d2c134a1c3d7ba7013
Joe Heyes <josephheyes@gmail.com>
2024-01-28 10:31:22 +0000

    me when i don't pattern match

--- ye-olde-src/main.py
+++ ye-olde-src/main.py
@@ -12,21 +12,20 @@ def main():
     # pause()
     while True:
         key = input()
-        match key:
-            case 'w':
-                magna_karta.forward()
-            case 's':
-                magna_karta.backward()
-            case 'a':
-                magna_karta.left()
-            case 'd':
-                magna_karta.right()
-            case 'q':
-                magna_karta.front.close()
-                magna_karta.back.close()
-                quit()
-            case _:
-                continue
+        if key == 'w':
+            magna_karta.forward()
+        elif key == 's':
+            magna_karta.backward()
+        elif key == 'a':
+            magna_karta.left()
+        elif key == 'd':
+            magna_karta.right()
+        elif key ==  'q':
+            magna_karta.front.close()
+            magna_karta.back.close()
+            quit()
+        else:
+            continue
 
 def avoid_collision():
     global magna_karta
